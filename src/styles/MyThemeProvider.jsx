5eb4ebdf553478618f72c7831cec37d31b3f81c6
DiegoCapella <diegocapelladev@gmail.com>
2022-06-11 15:12:46 -0300

    chore: first commit

---
+++ src/styles/MyThemeProvider.jsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+import P from 'prop-types';
+
+import { theme } from './theme';
+import { GlobalStyles } from './global-styles';
+
+export const MyThemeProvider = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+};
+
+MyThemeProvider.propTypes = {
+  children: P.node.isRequired,
+};
---
+++ src/templates/App/index.jsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+const App = () => {
+  return (
+    <S.Wrapper>
+      <div>My App</div>
+    </S.Wrapper>
+  )
+}
+
+export default App
