a0590f361b15eb808b2e73730ec1be2b08366c45
DiegoCapella <diegocapelladev@gmail.com>
2022-06-30 18:15:28 -0300

    style: config fonts

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,6 +1,76 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+  /* poppins-300 - latin */
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 300;
+      font-display: swap;
+      src: local(''),
+          url('../fonts/poppins-v20-latin-300.woff2') format('woff2')
+    }
+
+    /* poppins-regular - latin */
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: local(''),
+          url('../fonts/poppins-v20-latin-regular.woff2') format('woff2')
+    }
+
+    /* poppins-500 - latin */
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 500;
+      font-display: swap;
+      src: local(''),
+          url('../fonts/poppins-v20-latin-500.woff2') format('woff2')
+    }
+
+    /* poppins-600 - latin */
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 600;
+      font-display: swap;
+      src: local(''),
+          url('../fonts/poppins-v20-latin-600.woff2') format('woff2')
+    }
+
+    /* poppins-700 - latin */
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 700;
+      font-display: swap;
+      src: local(''),
+          url('../fonts/poppins-v20-latin-700.woff2') format('woff2')
+    }
+
+    /* poppins-800 - latin */
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 800;
+      font-display: swap;
+      src: local(''),
+          url('../fonts/poppins-v20-latin-800.woff2') format('woff2')
+    }
+
+    /* poppins-900 - latin */
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 900;
+      font-display: swap;
+      src: local(''),
+          url('../fonts/poppins-v20-latin-900.woff2') format('woff2')
+    }
+
   * {
     margin: 0;
     padding: 0;
@@ -22,8 +92,6 @@ export const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
-
-
     }
   `}
 `
